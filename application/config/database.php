44890f67f9a7ca44908419331ba1a32f37642b89
Ryukuko <alifrajaf@gmail.com>
2023-11-20 11:04:25 +0700

    pergantian database

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['api_sekolah'] = array(
+$db['ci_api'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
